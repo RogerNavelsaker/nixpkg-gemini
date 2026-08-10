dc697a1d6e993714955f49881285336b6042dfac
RogerNavelsaker <2011284+RogerNavelsaker@users.noreply.github.com>
2026-08-10 02:37:45 +0000

    chore: update sync mechanism to fix upstream updates

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       flake = false;
     };
     gemini-cli-nightly-src = {
-      url = "github:google-gemini/gemini-cli/v0.56.0-nightly.20260809.gcf22ac7e8";
+      url = "github:google-gemini/gemini-cli/v0.56.0-nightly.20260810.gcf22ac7e8";
       flake = false;
     };
     gemini-cli-stable-src = {
